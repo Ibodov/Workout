fadbe383373434a1903ab1dc7e5f0bccaaf9a617
Parviz Ibodov <ibodovparviz@gmail.com>
2021-03-25 16:45:38 +0500

    Изменил наследования с import androidx.fragment.app.Fragment; на import android.app.Fragment;

--- app/src/main/java/ibodov/smile/workout/MainActivity.java
+++ app/src/main/java/ibodov/smile/workout/MainActivity.java
@@ -1,14 +1,17 @@
 package ibodov.smile.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
